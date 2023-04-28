0753d5806c2a50b439bc8d596158e8c57905a78c
Frixes <tianputra30@gmail.com>
2023-04-28 19:29:23 +0700

    Final

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ const playerImg = document.getElementById("player-img");
 const compImg = document.getElementById("comp-img");
 const playerScore = document.getElementById("player-scr");
 const compScore = document.getElementById("comp-scr");
+const modal = document.getElementById("mymodal");
+const modalMsg = document.getElementById("modal-msg");
+const playBtn = document.getElementById("play-btn");
 
 function getComputerChoice(){
     return myArray[Math.floor(Math.random() * 3)]
@@ -57,10 +60,29 @@ function choice(playerChoice) {
     changeImg(playerSelection, computerSelection);
     playerScore.innerHTML = `player Score : ${playerCount}`;
     compScore.innerHTML = `computer Score : ${computerCount}`;
+
+    if (playerCount === 5) {
+        modal.style.display = "block";
+        modalMsg.innerHTML = "You Won";
+    } else if (computerCount === 5) {
+        modal.style.display = "block";
+        modalMsg.innerHTML = "You Lost";
+    }
 }
 
+function reset() {
+    playerCount = 0;
+    computerCount = 0;
+    playerScore.innerHTML = `player Score : ${playerCount}`;
+    compScore.innerHTML = `computer Score : ${computerCount}`;
+    winMsg.innerHTML = "first to score 5 wins";
+    playerImg.src = "./Image/question.png";
+    compImg.src = "./Image/question.png";
+    modal.style.display = "none";
+}
 
 rockBtn.addEventListener("click", () => choice("rock"));
 paperBtn.addEventListener("click", () => choice("paper"));
 scissorBtn.addEventListener("click", () => choice("scissor"));
+playBtn.addEventListener("click", reset);
 
